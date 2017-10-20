38a7913e46316ce2a894ec6883be4258cf3436b7
Dana Basken <dbasken@gmail.com>
2017-10-19 20:26:42 -0400

    adjusted asteroid forces to make it a bit harder at higher levels, asteroids now increasingly target pads as level increases

--- Assets/scripts/Asteroid.cs
+++ Assets/scripts/Asteroid.cs
@@ -13,12 +13,13 @@ public class Asteroid : MonoBehaviour {
 	public float xStartOffset = 3.0f;
 	public float yParticleShutoff = 3.0f;
 	public float yParticleDecayRate = 0.01f;
-	public float difficultyDownForce = 3.0f;
-	public float downForce = 10f;
+	public float difficultyDownForce = 20.0f;
+	public float downForce = 60.0f;
 
 	ParticleSystem particles;
 	bool particlesEnabled = false;
 	float sideForce;
+	int difficultyLevel;
 
 	float targetAngle;
 	Vector3 targetPosition;
@@ -30,13 +31,15 @@ public class Asteroid : MonoBehaviour {
 		particles = GetComponentInChildren<ParticleSystem>();
 		particlesEnabled = true;
 
+		difficultyLevel = GameManager.instance.getDifficultyLevel();
+
 		setupInitialPosition();
 	}
 	
 	void Update() {
 
-		// accelerate towards the target
-		float force = downForce + (GameManager.instance.getDifficultyLevel() * difficultyDownForce);
+		// accelerate towards the target (taking into account the difficulty level, to speed up the asteroid)
+		float force = downForce + (difficultyLevel * difficultyDownForce);
 		rigidBody.AddForce(rigidBody.transform.up * Time.deltaTime * -force);
 
 		// turn off the fireball effect if we're below a certain altitude
@@ -62,18 +65,46 @@ public class Asteroid : MonoBehaviour {
 		Destroy(gameObject);
 	}
 	void setupInitialPosition() {
+
 		// position the asteroid at its starting x position
 		float xStart = Random.Range(-xStartOffset, xStartOffset);
 		rigidBody.transform.position = new Vector3((float)xStart, yStart, 0f);
 
 		// find a target on the ground
-		targetPosition = new Vector3(Random.Range(-xStartOffset, xStartOffset), yEnd, 0f);
+		if (Random.Range(0, difficultyLevel) == 0) {
+			Debug.Log("TARGETING RANDOMLY");
+			targetRandomly();
+		} else {
+			Debug.Log("TARGETING SPECIFICALLY");
+			if (!targetSpecifically()) {
+				Debug.Log("TARGETING RANDOMLY BECAUSE SPECIFIC TARGETING FAILED");
+				targetRandomly();
+			}
+		}
 
 		// rotate the asteroid towards the target
 		Vector3 direction = transform.position - targetPosition;
 		direction.Normalize();
 		targetAngle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90f;
 		transform.rotation = Quaternion.Euler(0f, 0f, targetAngle);
+
+	}
+
+	void targetRandomly() {
+		targetPosition = new Vector3(Random.Range(-xStartOffset, xStartOffset), yEnd, 0f);
+	}
+
+	bool targetSpecifically() {
+		List<GameObject> pads = new List<GameObject>();
+		pads.AddRange(GameObject.FindGameObjectsWithTag("Pad"));
+		pads.AddRange(GameObject.FindGameObjectsWithTag("BrokenPad"));
+		if (pads.Count > 0) {
+			int padIndex = Random.Range(0, pads.Count);
+			targetPosition = pads[padIndex].transform.position;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	void explode() {
